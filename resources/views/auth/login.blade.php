3c31dd0895a221a2fa004e00a6fcb07d4433da09
Lama Sonmez <lamasonmez@gmail.com>
2020-11-03 13:52:12 +0200

    modify login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,208 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <!--ICON-->
+    <link rel="icon" href="<%= BASE_URL %>favicon.ico">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap" rel="stylesheet">
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+    <!-- Styles -->
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+<body id="kt_body" style="background-image: url(/metronic/theme/html/demo2/dist/assets/media/bg/bg-10.jpg)" class="quick-panel-right demo-panel-right offcanvas-right header-fixed subheader-enabled page-loading">
+    <div class="d-flex flex-column flex-root">
+        <!--begin::Login-->
+        <div class="login login-2 login-signin-on d-flex flex-column flex-lg-row flex-column-fluid bg-white" id="kt_login">
+            <!--begin::Aside-->
+            <div class="login-aside order-2 order-lg-1 d-flex flex-row-auto position-relative overflow-hidden">
+                <!--begin: Aside Container-->
+                <div class="d-flex flex-column-fluid flex-column justify-content-between py-9 px-7 py-lg-13 px-lg-35">
+                    <!--begin::Logo-->
+                    <a href="#" class="text-center pt-2">
+                        <img src="/metronic/theme/html/demo2/dist/assets/media/logos/logo.png" class="max-h-75px" alt="" />
+                    </a>
+                    <!--end::Logo-->
+                    <!--begin::Aside body-->
+                    <div class="d-flex flex-column-fluid flex-column flex-center">
+                        <!--begin::Signin-->
+                        <div class="login-form login-signin py-11">
+                            <!--begin::Form-->
+                            <form class="form" method="POST" action="{{ route('login') }}" id="kt_login_signin_form">
+                                @csrf
+                                <!--begin::Title-->
+                                <div class="text-center pb-8">
+                                    <h2 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">تسجيل دخول</h2>
+                                    <span class="text-muted font-weight-bold font-size-h4">أو
+                                    <a href="" class="text-primary font-weight-bolder" id="kt_login_signup">إنشاء حساب جديد</a></span>
                                 </div>
-                            </div>
+                                <!--end::Title-->
+                                <!--begin::Form group-->
+                                <div class="form-group">
+                                    <label class="font-size-h6 font-weight-bolder text-dark">البردي الالكتروني</label>
+                                    <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg @error('email') is-invalid  @enderror"
+                                           type="text"
+                                           name="username"
+                                           value="{{ old('email') }}"
+                                           required
+                                           autocomplete="off" />
+                                    @error('email')
+                                    <div class="fv-plugins-message-container">
+                                        <div  class="fv-help-block">{{ $message }}</div>
+                                    </div>
+                                    @enderror
+                                </div>
+                                <!--end::Form group-->
+                                <!--begin::Form group-->
+                                <div class="form-group">
+                                    <div class="d-flex justify-content-between mt-n5">
+                                        <label class="font-size-h6 font-weight-bolder text-dark pt-5">كلمة المرور</label>
+                                        <a href="javascript:;" class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5" id="kt_login_forgot">نسيت كلمة المرور ?</a>
+                                    </div>
+                                    <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg" type="password" name="password" required autocomplete="off" />
+                                </div>
+                                <!--end::Form group-->
+                                <!--begin::Action-->
+                                <div class="text-center pt-2">
+                                    <button id="kt_login_signin_submit" class="btn btn-dark font-weight-bolder font-size-h6 px-8 py-4 my-3">تسجيل الدخول</button>
+                                </div>
+                                <!--end::Action-->
+                            </form>
+                            <!--end::Form-->
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <!--end::Signin-->
+                        <!--begin::Signup-->
+                        <div class="login-form login-signup pt-11">
+                            <!--begin::Form-->
+                            <form class="form" method="POST" action="{{ route('register') }}" id="kt_login_signup_form">
+                                @csrf
+                                <!--begin::Title-->
+                                <div class="text-center pb-8">
+                                    <h2 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">إنشاء حساب جديد</h2>
+                                    <p class="text-muted font-weight-bold font-size-h4">الر جاء ادخال المعلومات التالية لإنشاء حساب جديد</p>
+                                </div>
+                                <!--end::Title-->
+                                <!--begin::Form group-->
+                                <div class="form-group">
+                                    <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6 @error('name') is-invalid @enderror" type="text" placeholder="الاسم بالكامل" name="name" value="{{ old('name') }}" required autofocus />
+                                    @error('name')
+                                    <div class="fv-plugins-message-container">
+                                        <div  class="fv-help-block">{{ $message }}</div>
+                                    </div>
+                                    @enderror
+                                </div>
+                                <!--end::Form group-->
+                                <!--begin::Form group-->
+                                <div class="form-group">
+                                    <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6 @error('email') is-invalid @enderror" type="البريد الالكتروني" placeholder="Email" name="email" required value="{{ old('email') }}" autocomplete="off" />
+                                    @error('email')
+                                    <div class="fv-plugins-message-container">
+                                        <div  class="fv-help-block">{{ $message }}</div>
+                                    </div>
+                                    @enderror
+                                </div>
+                                <!--end::Form group-->
+                                <!--begin::Form group-->
+                                <div class="form-group">
+                                    <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6" type="password" placeholder="كلمة المرور" name="password" required autocomplete="off" />
+                                    @error('password')
+                                    <div class="fv-plugins-message-container">
+                                        <div  class="fv-help-block">{{ $message }}</div>
+                                    </div>
+                                    @enderror
+                                </div>
+                                <!--end::Form group-->
+                                <!--begin::Form group-->
+                                <div class="form-group">
+                                    <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6" type="password" placeholder="تأكيد كلمة المرور" name="password_confirmation" required autocomplete="new-password" />
+                                </div>
+                                <!--end::Form group-->
+                                <!--begin::Form group-->
+                                <div class="form-group d-flex flex-wrap flex-center pb-lg-0 pb-3">
+                                    <button type="button" id="kt_login_signup_submit" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mx-4" type="submit">إرسال</button>
+                                    <button type="button" id="kt_login_signup_cancel" class="btn btn-light-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mx-4">إلغاء</button>
+                                </div>
+                                <!--end::Form group-->
+                            </form>
+                            <!--end::Form-->
                         </div>
-                    </form>
+                        <!--end::Signup-->
+                        <!--begin::Forgot-->
+                        <div class="login-form login-forgot pt-11">
+                            <!--begin::Form-->
+                            <form class="form" method="POST" action="{{ route('password.email') }}" id="kt_login_forgot_form">
+                                @csrf
+
+                                <!--begin::Title-->
+                                <div class="text-center pb-8">
+                                    <h2 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">نسيت كلمة المررو ?</h2>
+                                    <p class="text-muted font-weight-bold font-size-h4">الرجاء إدخال بريدك الالكتروني من أجل تهيئة كلمة المرور</p>
+                                </div>
+                                <!--end::Title-->
+                                <!--begin::Form group-->
+                                <div class="form-group">
+                                    <input class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6 @error('email') is-invalid @enderror" type="email" placeholder="البريد الالكتروني" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus />
+                                     @error('email')
+                                     <div class="fv-plugins-message-container">
+                                         <div  class="fv-help-block">{{ $message }}</div>
+                                     </div>
+                                     @enderror
+                                </div>
+                                <!--end::Form group-->
+                                <!--begin::Form group-->
+                                <div class="form-group d-flex flex-wrap flex-center pb-lg-0 pb-3">
+                                    <button type="button" id="kt_login_forgot_submit" class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mx-4" type="submit">إرسال</button>
+                                    <button type="button" id="kt_login_forgot_cancel" class="btn btn-light-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mx-4">إالغاء</button>
+                                </div>
+                                <!--end::Form group-->
+                            </form>
+                            <!--end::Form-->
+                        </div>
+                        <!--end::Forgot-->
+                    </div>
+                    <!--end::Aside body-->
+
                 </div>
+                <!--end: Aside Container-->
             </div>
+            <!--begin::Aside-->
+            <!--begin::Content-->
+            <div class="content order-1 order-lg-2 d-flex flex-column w-100 pb-0" style="background-color: #B1DCED;">
+                <!--begin::Title-->
+                <div class="d-flex flex-column justify-content-center text-center pt-lg-40 pt-md-5 pt-sm-5 px-lg-0 pt-5 px-7">
+                    <h3 class="display4 font-weight-bolder my-7 text-dark" style="color: #986923;">Vendor System</h3>
+                    <p class="font-weight-bolder font-size-h2-md font-size-lg text-dark opacity-70">Some Text Here                    <br />Web Application & Advanced Solutions</p>
+                </div>
+                <!--end::Title-->
+                <!--begin::Image-->
+                <div class="content-img d-flex flex-row-fluid bgi-no-repeat bgi-position-y-bottom bgi-position-x-center" style="background-image: url({{ asset('/images/login-visual-2.svg') }});"></div>
+                <!--end::Image-->
+            </div>
+            <!--end::Content-->
         </div>
+        <!--end::Login-->
     </div>
-</div>
-@endsection
+
+    <script src="{{ asset('/js/plugins.bundle.js') }}"></script>
+    <script src="{{ asset('/js/prismjs.bundle.js') }}"></script>
+    <script src="{{ asset('/js/scripts.bundle.js') }}"></script>
+    <script src="{{ asset('/js/login.js') }}"></script>
+</body>
+</html>
+
