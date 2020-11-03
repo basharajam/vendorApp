09604b974c0e45cff6a0183e148e9f6f45237a6d
Lama Sonmez <lamasonmez@gmail.com>
2020-11-03 10:45:53 +0200

    add User Roles Constatns

---
+++ app/Constants/UserRoles.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Constants;
+
+class UserRoles  {
+
+    CONST VENDOR = 'vendor';
+    CONST VENDORADMIN = 'vendor_admin';
+
+    CONST ALL_ROLES=[
+        self::VENDOR,
+        self::VENDORADMIN,
+    ];
+
+
+}
