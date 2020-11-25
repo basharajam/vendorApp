a8fcd3aff480ecc2900aa936edfba0391bcbf586
Lama Sonmez <lamasonmez@gmail.com>
2020-11-25 12:59:45 +0200

    add register controller for supplier manager

---
+++ app/Http/Controllers/SupplierManager/RegisterController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\SupplierManager;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Supplier\StoreSupplierRequest;
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+
+
+}
