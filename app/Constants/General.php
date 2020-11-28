33fa1b1728a913cc30f5c3e5becf562f03734581
Lama Sonmez <lamasonmez@gmail.com>
2020-11-28 12:14:27 +0200

    change General Url

--- app/Constants/General.php
+++ app/Constants/General.php
@@ -4,6 +4,8 @@ namespace App\Constants;
 class General  {
 
     // CONST URL = 'https://alyamanlive.com';
-    CONST URL = 'http://127.0.0.1:8000';
+    CONST URL = 'https://vendor.alyamanlive.ecom';
+    // CONST URL = 'https://test.alymanalive.com';
+    // CONST URL = 'http://127.0.0.1:8000';
 
 }
