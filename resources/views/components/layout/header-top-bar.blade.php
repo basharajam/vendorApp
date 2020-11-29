357e4a0e1cd8e317839a7bcdeab2cf1f92d82a18
Lama Sonmez <lamasonmez@gmail.com>
2020-11-29 14:36:37 +0200

    modify header-top-bar

--- resources/views/components/layout/header-top-bar.blade.php
+++ resources/views/components/layout/header-top-bar.blade.php
@@ -1,6 +1,11 @@
 <div>
     <div class="kt-header__top ">
         <div class="kt-container kt-container--fluid ">
+            <!-- begin:: Header Topbar -->
+            <div class="kt-header__topbar" >
+                <x-layout.header-user-bar/>
+            </div>
+            <!-- end:: Header Topbar -->
             <!-- begin:: Brand -->
             <div class="kt-header__brand   kt-grid__item" id="kt_header_brand" style="margin: auto">
                 <div class="kt-header__brand-nav">
@@ -8,11 +13,7 @@
                 </div>
             </div>
             <!-- end:: Brand -->
-            <!-- begin:: Header Topbar -->
-            <div class="kt-header__topbar">
-                <x-layout.header-user-bar/>
-            </div>
-            <!-- end:: Header Topbar -->
+
         </div>
     </div>
 </div>
--- resources/views/components/layout/header-user-bar.blade.php
+++ resources/views/components/layout/header-user-bar.blade.php
@@ -1,5 +1,5 @@
-<div class="kt-header__topbar-item kt-header__topbar-item--user">
-    <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="10px,10px" aria-expanded="false">
+<div class="kt-header__topbar-item kt-header__topbar-item--user" >
+    <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="10px,10px" aria-expanded="false" style="flex-direction: row-reverse">
         <span class="kt-header__topbar-welcome"> </span>
             <span class="kt-header__topbar-username ml-5 font-size-h4" v-if="user">{{ \Auth::user()->name }}</span>
             <span class="kt-badge kt-badge--username kt-badge--unified-success kt-badge--lg kt-badge--rounded ">
@@ -8,23 +8,7 @@
     </div>
     <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-xl">
         <!--begin: Head -->
-        <div class="kt-user-card kt-user-card--skin-dark kt-notification-item-padding-x">
-                    <div class="kt-user-card__avatar">
-                        <span class="kt-badge kt-badge--lg kt-badge--rounded kt-badge--bold kt-font-success ">L</span>
-                    </div>
-                    <div class="kt-user-card__name" v-if="user">
-                        Lama Sonmez
-                    </div>
-
-        </div>
         <div class="kt-notification">
-                    <a href="#" class="kt-notification__item">
-                        <div class="kt-notification__item-icon"><i class="flaticon2-calendar-3 kt-font-success"></i></div>
-                        <div class="kt-notification__item-details">
-                            <div class="kt-notification__item-title kt-font-bold">My Profile</div>
-                            <div class="kt-notification__item-time"></div>
-                        </div>
-                    </a>
             <div class="kt-notification__custom kt-space-between">
                 <a href="{{ route('logout') }}"
                 onclick="event.preventDefault();
