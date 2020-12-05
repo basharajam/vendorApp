ea63ccca125b0935d16efd4f8807a43db832494d
Lama Sonmez <lamasonmez@gmail.com>
2020-12-05 10:31:47 +0200

    fix responsive design for taxonimies page

--- resources/views/supplier/taxonomies/components/taxonomy_table.blade.php
+++ resources/views/supplier/taxonomies/components/taxonomy_table.blade.php
@@ -19,76 +19,124 @@
         </div>
     </div>
     <div class="kt-portlet__body">
-        <table id="GridTaxnomies" class="table" style="direction:rtl;text-align:right">
-            <thead class="thead-light">
-                <tr>
-                    @if($type=='product_cat')
-                    <th>الصورة</th>
-                    @endif
-                    <th>الاسم</th>
-                    <th>الوصف</th>
-                    <th>slug</th>
-                    <th>العدد</th>
-                    <th>..</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($data as $taxonomy)
-                    <tr id="{{ $taxonomy->term_taxonomy_id }}">
+        <div class="table-responsive">
+            <table id="GridTaxnomies" class="table table-bordered" style="direction:rtl;text-align:right">
+                <thead class="thead-light">
+                    <tr>
                         @if($type=='product_cat')
-                        <td class="datatable-cell">
-                            <span style="width: 250px;">
-                                <div class="d-flex align-items-center">
-                                    <div style="padding-left:10px;" class="symbol symbol-50 symbol-sm symbol-light-danger">
-                                        <span class="symbol-label font-size-p" style="background-image:url({{ $taxonomy->image ?? '' }})"></span>
-                                    </div>
-                                </div>
-                            </span>
-                        </td>
+                        <th>الصورة</th>
                         @endif
-                        <td class="datatable-cell-sorted datatable-cell">
-                            <span>
-                                <div class="font-weight-bolder font-size-lg mb-0">
-                                     {{ $taxonomy->term->name }}
-                                </div>
-                            </span>
-                        </td>
-                        <td class="datatable-cell-sorted datatable-cell">
-                            <span>
-                                <div class="font-weight-bolder font-size-lg mb-0">
-                                     {{ $taxonomy->term->description  ?? '--'}}
-                                </div>
-                            </span>
-                        </td>
-                        <td class="datatable-cell-sorted datatable-cell">
-                            <span>
-                                <div class="font-weight-bolder font-size-lg mb-0">
-                                     {{ $taxonomy->term->slug }}
-                                </div>
-                            </span>
-                        </td>
-                        <td class="datatable-cell-sorted datatable-cell">
-                            <span>
-                                <div class="font-weight-bolder font-size-lg mb-0">
-                                     {{ count($taxonomy->posts) }}
-                                </div>
-                            </span>
-                        </td>
-                        <td class="datatable-cell-sorted datatable-cell" style="min-width:100px;">
-                            <a id="{{ $taxonomy->term_taxonomy_id }}" class="kt-nav__link mr-5 delete" data-action-name="{{ route('supplier.taxonomies.delete',$taxonomy->term_taxonomy_id) }}" href="javascript:;"  ><i class="kt-nav__link-icon flaticon2-trash "></i></a>
-                            <a class="kt-nav__link edit_taxonomy" data-type="{{ $type }}" id="{{ $taxonomy->term_taxonomy_id }}" data-action-name="{{ route('supplier.taxonomies.edit') }}" href="#"  ><i class="kt-nav__link-icon color-primary flaticon-edit-1 "></i></a>
-                        </td>
+                        <th>الاسم</th>
+                        <th>الوصف</th>
+                        <th>slug</th>
+                        <th>العدد</th>
+                        <th>..</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($data as $taxonomy)
+                        <tr id="{{ $taxonomy->term_taxonomy_id }}">
+                            @if($type=='product_cat')
+                            <td class="datatable-cell">
+                                <span style="width: 250px;">
+                                    <div class="d-flex align-items-center">
+                                        <div style="padding-left:10px;" class="symbol symbol-50 symbol-sm symbol-light-danger">
+                                            <span class="symbol-label font-size-p" style="background-image:url({{ $taxonomy->image ?? '' }})"></span>
+                                        </div>
+                                    </div>
+                                </span>
+                            </td>
+                            @endif
+                            <td class="datatable-cell-sorted datatable-cell">
+                                <span>
+                                    <div class="font-weight-bolder font-size-lg mb-0">
+                                        {{ $taxonomy->term->name }}
+                                    </div>
+                                </span>
+                            </td>
+                            <td class="datatable-cell-sorted datatable-cell">
+                                <span>
+                                    <div class="font-weight-bolder font-size-lg mb-0">
+                                        {{ $taxonomy->term->description  ?? '--'}}
+                                    </div>
+                                </span>
+                            </td>
+                            <td class="datatable-cell-sorted datatable-cell">
+                                <span>
+                                    <div class="font-weight-bolder font-size-lg mb-0">
+                                        {{ $taxonomy->term->slug }}
+                                    </div>
+                                </span>
+                            </td>
+                            <td class="datatable-cell-sorted datatable-cell">
+                                <span>
+                                    <div class="font-weight-bolder font-size-lg mb-0">
+                                        {{ count($taxonomy->posts) }}
+                                    </div>
+                                </span>
+                            </td>
+                            <td class="datatable-cell-sorted datatable-cell" style="min-width:100px;">
+                                <a id="{{ $taxonomy->term_taxonomy_id }}" class="kt-nav__link mr-5 delete" data-action-name="{{ route('supplier.taxonomies.delete',$taxonomy->term_taxonomy_id) }}" href="javascript:;"  ><i class="kt-nav__link-icon flaticon2-trash "></i></a>
+                                <a class="kt-nav__link edit_taxonomy" data-type="{{ $type }}" id="{{ $taxonomy->term_taxonomy_id }}" data-action-name="{{ route('supplier.taxonomies.edit') }}" href="#"  ><i class="kt-nav__link-icon color-primary flaticon-edit-1 "></i></a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 
 @push('scripts')
 <script>
     $(document).ready(function() {
-    $('#GridTaxnomies').DataTable();
+    $('#GridTaxnomies').DataTable({
+        scrollCollapse: true,
+        language:{
+            "emptyTable": "ليست هناك بيانات متاحة في الجدول",
+            "loadingRecords": "جارٍ التحميل...",
+            "processing": "جارٍ التحميل...",
+            "lengthMenu": "أظهر _MENU_ مدخلات",
+            "zeroRecords": "لم يعثر على أية سجلات",
+            "info": "إظهار _START_ إلى _END_ من أصل _TOTAL_ مدخل",
+            "infoEmpty": "يعرض 0 إلى 0 من أصل 0 سجل",
+            "infoFiltered": "(منتقاة من مجموع _MAX_ مُدخل)",
+            "search": "ابحث:",
+            "paginate": {
+                "first": "الأول",
+                "previous": "السابق",
+                "next": "التالي",
+                "last": "الأخير"
+            },
+            "aria": {
+                "sortAscending": ": تفعيل لترتيب العمود تصاعدياً",
+                "sortDescending": ": تفعيل لترتيب العمود تنازلياً"
+            },
+            "select": {
+                "rows": {
+                    "_": "%d قيمة محددة",
+                    "0": "",
+                    "1": "1 قيمة محددة"
+                }
+            },
+            "buttons": {
+                "print": "طباعة",
+                "colvis": "الأعمدة الظاهرة",
+                "copy": "نسخ إلى الحافظة",
+                "copyTitle": "نسخ",
+                "copyKeys": "زر <i>ctrl<\/i> أو <i>⌘<\/i> + <i>C<\/i> من الجدول<br>ليتم نسخها إلى الحافظة<br><br>للإلغاء اضغط على الرسالة أو اضغط على زر الخروج.",
+                "copySuccess": {
+                    "_": "%d قيمة نسخت",
+                    "1": "1 قيمة نسخت"
+                },
+                "pageLength": {
+                    "-1": "اظهار الكل",
+                    "_": "إظهار %d أسطر"
+                }
+            }
+        }
+
+    });
 } );
 </script>
     <script>
--- resources/views/supplier/taxonomies/index.blade.php
+++ resources/views/supplier/taxonomies/index.blade.php
@@ -2,30 +2,25 @@
 
 @section('content')
 <div class="kt-container kt-container--fluid kt-grid__item kt-grid__item--fluid">
-    <div class="kt-grid kt-grid--desktop kt-grid--ver kt-grid--ver-desktop kt-app">
-         <!--Begin:: App Aside Mobile Toggle-->
-         <button class="kt-app__aside-close" id="kt_user_profile_aside_close">
-            <i class="la la-close"></i>
-        </button>
-        <!--End:: App Aside Mobile Toggle-->
-
-        <!--Begin:: App Aside-->
-        <div class="kt-grid__item kt-app__toggle kt-app__aside" id="kt_user_profile_aside">
-            <div class="kt-portlet kt-portlet--height-fluid-">
-                @include('supplier.taxonomies.components.add_card')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-12">
+                <div class="kt-portlet kt-portlet--height-fluid-">
+                    @include('supplier.taxonomies.components.add_card')
+                </div>
             </div>
-        </div>
-        <!--End:: App Aside-->
-
-        <!--Begin:: App Content-->
-        <div class="kt-grid__item kt-grid__item--fluid kt-app__content mr-10">
-            <div class="row">
-                <div class="col-xl-12">
-                    @include('supplier.taxonomies.components.taxonomy_table')
+            <div class="col-md-8 col-sm-6 col-12">
+                <div class="kt-grid__item kt-grid__item--fluid kt-app__content">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            @include('supplier.taxonomies.components.taxonomy_table')
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
 
 </div>
 @endsection
