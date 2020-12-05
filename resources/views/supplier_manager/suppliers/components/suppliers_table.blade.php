857090eb017b1ee00ac5fc47738a260cc12ef18e
Lama Sonmez <lamasonmez@gmail.com>
2020-12-05 11:17:11 +0200

    fix suppliers page responsive design

--- resources/views/supplier_manager/suppliers/components/suppliers_table.blade.php
+++ resources/views/supplier_manager/suppliers/components/suppliers_table.blade.php
@@ -1,71 +1,118 @@
-<table id="SuppliersTable" class="table" style="direction:rtl;text-align:right">
-    <thead class="thead-light">
-        <tr>
-            <th class="">
-                <span>اسم المورد</span>
-                <div class="font-weight-bold text-muted">Supplier Name</div>
-            </th>
-            <th class="">
-                <span>اسم الشركة</span>
-                <div class="font-weight-bold text-muted">Supplier Company</div>
-            </th>
-            <th class="">
-                <span>البريد الالكتروني</span>
-                <div class="font-weight-bold text-muted">Email</div>
-            </th>
-            <th class="">
-                <span>اسم المحل</span>
-                <div class="font-weight-bold text-muted">Shop Address</div>
-            </th>
-            <th class="">
-                <span>الجنسية</span>
-                <div class="font-weight-bold text-muted">Nationality</div>
-            </th>
+<div class="table-responsive">
+    <table id="SuppliersTable" class="table" style="direction:rtl;text-align:right">
+        <thead class="thead-light">
+            <tr>
+                <th class="">
+                    <span>اسم المورد</span>
+                    <div class="font-weight-bold text-muted">Supplier Name</div>
+                </th>
+                <th class="">
+                    <span>اسم الشركة</span>
+                    <div class="font-weight-bold text-muted">Supplier Company</div>
+                </th>
+                <th class="">
+                    <span>البريد الالكتروني</span>
+                    <div class="font-weight-bold text-muted">Email</div>
+                </th>
+                <th class="">
+                    <span>اسم المحل</span>
+                    <div class="font-weight-bold text-muted">Shop Address</div>
+                </th>
+                <th class="">
+                    <span>الجنسية</span>
+                    <div class="font-weight-bold text-muted">Nationality</div>
+                </th>
 
-            <th class="">
-                <span>تاريخ الاضافة</span>
-                <div class="font-weight-bold text-muted">Created At</div>
-            </th>
-            <th>
-                ..
-            </th>
+                <th class="">
+                    <span>تاريخ الاضافة</span>
+                    <div class="font-weight-bold text-muted">Created At</div>
+                </th>
+                <th>
+                    ..
+                </th>
 
-        </tr>
-    </thead>
-    <tbody style="direction:rtl;text-align:right">
-        @foreach($suppliers as $supplier)
-            <tr id="{{ $supplier->id }}">
-                <td>
-                    <span>{{ $supplier->full_name }}</span>
-                </td>
-                <td>
-                    <span>{{ $supplier->company_name }}</span>
-                </td>
-                <td>
-                    <span>{{ $supplier->eamil }}</span>
-                </td>
-                <td>
-                    <span>{{ $supplier->shop_address }}</span>
-                </td>
-                <td>
-                    <span>{{ $supplier->nationality }}</span>
-                </td>
-                <td>
-                    <span>{{ date('Y-m-d',strtotime($supplier->created_at)) }}</span>
-                </td>
-                <td>
-                    <td class="datatable-cell-sorted datatable-cell">
-                        <a id="{{ $supplier->id }}" class="kt-nav__link mr-5 delete" data-action-name="{{ route('supplier_manager.suppliers.delete',$supplier->id) }}" href="javascript:;"  ><i class="kt-nav__link-icon flaticon2-trash "></i></a>
-                        <a class="kt-nav__link"  href="{{ route('supplier_manager.suppliers.edit',$supplier->id) }}"  ><i class="kt-nav__link-icon color-primary flaticon-edit-1 "></i></a>
-                    </td>
-                </td>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody style="direction:rtl;text-align:right">
+            @foreach($suppliers as $supplier)
+                <tr id="{{ $supplier->id }}">
+                    <td>
+                        <span>{{ $supplier->full_name }}</span>
+                    </td>
+                    <td>
+                        <span>{{ $supplier->company_name }}</span>
+                    </td>
+                    <td>
+                        <span>{{ $supplier->eamil }}</span>
+                    </td>
+                    <td>
+                        <span>{{ $supplier->shop_address }}</span>
+                    </td>
+                    <td>
+                        <span>{{ $supplier->nationality }}</span>
+                    </td>
+                    <td>
+                        <span>{{ date('Y-m-d',strtotime($supplier->created_at)) }}</span>
+                    </td>
+                    <td class="datatable-cell-sorted datatable-cell">
+                            <a id="{{ $supplier->id }}" class="kt-nav__link mr-5 delete" data-action-name="{{ route('supplier_manager.suppliers.delete',$supplier->id) }}" href="javascript:;"  ><i class="kt-nav__link-icon flaticon2-trash "></i></a>
+                            <a class="kt-nav__link"  href="{{ route('supplier_manager.suppliers.edit',$supplier->id) }}"  ><i class="kt-nav__link-icon color-primary flaticon-edit-1 "></i></a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @push('scripts')
 <script>
     $(document).ready(function() {
+        $('#SuppliersTable').DataTable({
+        scrollCollapse: true,
+        language:{
+            "emptyTable": "ليست هناك بيانات متاحة في الجدول",
+            "loadingRecords": "جارٍ التحميل...",
+            "processing": "جارٍ التحميل...",
+            "lengthMenu": "أظهر _MENU_ مدخلات",
+            "zeroRecords": "لم يعثر على أية سجلات",
+            "info": "إظهار _START_ إلى _END_ من أصل _TOTAL_ مدخل",
+            "infoEmpty": "يعرض 0 إلى 0 من أصل 0 سجل",
+            "infoFiltered": "(منتقاة من مجموع _MAX_ مُدخل)",
+            "search": "ابحث:",
+            "paginate": {
+                "first": "الأول",
+                "previous": "السابق",
+                "next": "التالي",
+                "last": "الأخير"
+            },
+            "aria": {
+                "sortAscending": ": تفعيل لترتيب العمود تصاعدياً",
+                "sortDescending": ": تفعيل لترتيب العمود تنازلياً"
+            },
+            "select": {
+                "rows": {
+                    "_": "%d قيمة محددة",
+                    "0": "",
+                    "1": "1 قيمة محددة"
+                }
+            },
+            "buttons": {
+                "print": "طباعة",
+                "colvis": "الأعمدة الظاهرة",
+                "copy": "نسخ إلى الحافظة",
+                "copyTitle": "نسخ",
+                "copyKeys": "زر <i>ctrl<\/i> أو <i>⌘<\/i> + <i>C<\/i> من الجدول<br>ليتم نسخها إلى الحافظة<br><br>للإلغاء اضغط على الرسالة أو اضغط على زر الخروج.",
+                "copySuccess": {
+                    "_": "%d قيمة نسخت",
+                    "1": "1 قيمة نسخت"
+                },
+                "pageLength": {
+                    "-1": "اظهار الكل",
+                    "_": "إظهار %d أسطر"
+                }
+            }
+        }
+
+    });
 } );
 </script>
 @endpush
