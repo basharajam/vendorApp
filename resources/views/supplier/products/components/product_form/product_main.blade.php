18f35ec910fbbf457d89e1f8acada9ffd6c645ef
Lama Sonmez <lamasonmez@gmail.com>
2020-12-06 14:48:34 +0200

    fix

--- resources/views/supplier/products/components/product_form/product_main.blade.php
+++ resources/views/supplier/products/components/product_form/product_main.blade.php
@@ -35,7 +35,9 @@
                             <span class="flaticon2-information" data-toggle="tooltip" data-theme="dark"  title="hi"></span>
                         </label>
                         <textarea id="editor" class="form-control @error('post_content') is-invalid @enderror" name="post_content" required>
+                            @if($product)
                             {{ $product->post_content}}
+                            @endif
                         </textarea>
                         @error('post_content')
                         <span class="invalid-feedback" role="alert">
