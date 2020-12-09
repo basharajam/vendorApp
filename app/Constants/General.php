21a756956c6948365c820b5c381c13a9f2a97352
Lama Sonmez <lamasonmez@gmail.com>
2020-12-09 12:29:50 +0200

    chagne Baseurl to test.alymanlive.com

--- app/Constants/General.php
+++ app/Constants/General.php
@@ -4,10 +4,10 @@ namespace App\Constants;
 class General  {
 
     // CONST URL = 'https://alyamanlive.com';
-    CONST URL = 'https://vendor.alyamanlive.com';
+    //CONST URL = 'https://vendor.alyamanlive.com';
     // CONST DB_PREFIX = "wpug_";
     CONST DB_PREFIX = "AL_";
-    // CONST URL = 'https://test.alymanalive.com';
+    CONST URL = 'https://test.alymanalive.com';
     // CONST URL = 'http://127.0.0.1:8000';
 
 }
