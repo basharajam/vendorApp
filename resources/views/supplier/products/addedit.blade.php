262ad812d531e8c0023e12f04c3cf61bfeff10ce
Lama Sonmez <lamasonmez@gmail.com>
2020-12-16 10:46:56 +0200

    fix

--- resources/views/supplier/products/addedit.blade.php
+++ resources/views/supplier/products/addedit.blade.php
@@ -162,10 +162,11 @@
                                     </div>
                                 </div>
                             </form>
+                            <div class="w-100">
+                                @include('supplier.products.components.product_form.variations_card_container')
+                            </div>
                         </div>
-                        <div class="w-100">
-                            @include('supplier.products.components.product_form.variations_card_container')
-                        </div>
+
                 </div>
             </div>
         </div>
