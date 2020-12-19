9de055b1bc8f3e3aae616772b131392930851b5e
Lama Sonmez <lamasonmez@gmail.com>
2020-12-19 14:20:36 +0200

    modify orderItemService

--- app/Services/OrderItem/OrderItemService.php
+++ app/Services/OrderItem/OrderItemService.php
@@ -9,6 +9,7 @@ use App\Repositories\OrderItemRepository;
 use App\Services\Contracts\BaseService;
 use App\Models\WP\Post;
 use App\Models\WP\OrderItem;
+use App\Models\WP\OrderItemMeta;
 
 //TODO Modify According to Supplier and SUpplier manager
 
@@ -36,8 +37,9 @@ class OrderItemService extends BaseService implements IOrderItemService
         $products_ids =Post::where('post_author',$supplier_id)
                             ->whereIn('post_type',['product','product_variation'])
                             ->get()->pluck('ID')->toArray();
-        $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
-        return OrderItem::where('order_item_type','line_item')->whereIn('order_id',$orders_ids)->get();
+        // $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
+        $orders_ids = OrderItemMeta::whereIn('meta_key',['_product_id','_variation_id'])->whereIn('meta_value',$products_ids)->get()->pluck('order_item_id')->toArray();
+        return OrderItem::where('order_item_type','line_item')->whereIn('order_item_id',$orders_ids)->get();
     }
     /** get's all orders for a s manger
      * @param $manager_id
@@ -52,8 +54,11 @@ class OrderItemService extends BaseService implements IOrderItemService
         $products_ids =Post::whereIn('post_author',$suppliers_ids)
         ->whereIn('post_type',['product','product_variation'])
         ->get()->pluck('ID')->toArray();
-        $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
-        return OrderItem::where('order_item_type','line_item')->whereIn('order_id',$orders_ids)->get();
+        // $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
+        $orders_ids = OrderItemMeta::whereIn('meta_key',['_product_id','_variation_id'])->whereIn('meta_value',$products_ids)->get()->pluck('order_item_id')->toArray();
+        // return OrderItem::where('order_item_type','line_item')->whereIn('order_id',$orders_ids)->get();
+        return OrderItem::where('order_item_type','line_item')->whereIn('order_item_id',$orders_ids)->get();
+
     }
     /** get paid orders for a supplier
      * @param $supplier_id
@@ -63,8 +68,10 @@ class OrderItemService extends BaseService implements IOrderItemService
         $products_ids =Post::where('post_author',$supplier_id)
         ->whereIn('post_type',['product','product_variation'])
         ->get()->pluck('ID')->toArray();
-        $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
-        return OrderItem::where('order_item_type','line_item')->whereIn('order_id',$orders_ids)->whereHas('post',function($query){
+        // $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
+        $orders_ids = OrderItemMeta::whereIn('meta_key',['_product_id','_variation_id'])->whereIn('meta_value',$products_ids)->get()->pluck('order_item_id')->toArray();
+
+        return OrderItem::where('order_item_type','line_item')->whereIn('order_item_id',$orders_ids)->whereHas('post',function($query){
             return $query->where('post_status','wc-completed');
         })->get();
 
@@ -82,8 +89,10 @@ class OrderItemService extends BaseService implements IOrderItemService
         $products_ids =Post::where('post_author',$suppliers_ids)
         ->whereIn('post_type',['product','product_variation'])
         ->get()->pluck('ID')->toArray();
-        $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
-        return OrderItem::where('order_item_type','line_item')->whereIn('order_id',$orders_ids)->whereHas('post',function($query){
+        // $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
+        $orders_ids = OrderItemMeta::whereIn('meta_key',['_product_id','_variation_id'])->whereIn('meta_value',$products_ids)->get()->pluck('order_item_id')->toArray();
+
+        return OrderItem::where('order_item_type','line_item')->whereIn('order_item_id',$orders_ids)->whereHas('post',function($query){
             return $query->where('post_status','wc-completed');
         })->get();
     }
@@ -95,8 +104,9 @@ class OrderItemService extends BaseService implements IOrderItemService
         $products_ids =Post::where('post_author',$supplier_id)
         ->whereIn('post_type',['product','product_variation'])
         ->get()->pluck('ID')->toArray();
-        $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
-        return OrderItem::where('order_item_type','line_item')->whereIn('order_id',$orders_ids)->whereHas('post',function($query){
+        // $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
+        $orders_ids = OrderItemMeta::whereIn('meta_key',['_product_id','_variation_id'])->whereIn('meta_value',$products_ids)->get()->pluck('order_item_id')->toArray();
+        return OrderItem::where('order_item_type','line_item')->whereIn('order_item_id',$orders_ids)->whereHas('post',function($query){
             return $query->where('post_status','!=','wc-completed');
         })->get();
 
@@ -114,8 +124,10 @@ class OrderItemService extends BaseService implements IOrderItemService
         $products_ids =Post::where('post_author',$suppliers_ids)
         ->whereIn('post_type',['product','product_variation'])
         ->get()->pluck('ID')->toArray();
-        $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
-        return OrderItem::where('order_item_type','line_item')->whereIn('order_id',$orders_ids)->whereHas('post',function($query){
+        // $orders_ids = OrderDetail::whereIn('product_id',$products_ids)->get()->pluck('order_id')->toArray();
+        $orders_ids = OrderItemMeta::whereIn('meta_key',['_product_id','_variation_id'])->whereIn('meta_value',$products_ids)->get()->pluck('order_item_id')->toArray();
+
+        return OrderItem::where('order_item_type','line_item')->whereIn('order_item_id',$orders_ids)->whereHas('post',function($query){
             return $query->where('post_status','!=','wc-completed');
         })->get();
     }
