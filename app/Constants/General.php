7759982ebc58d3d65d9de1e37287404b0f07d136
Lama Sonmez <lamasonmez@gmail.com>
2020-12-20 22:31:10 +0200

    db prrfix

--- app/Constants/General.php
+++ app/Constants/General.php
@@ -6,7 +6,7 @@ class General  {
     // CONST URL = 'https://alyamanlive.com';
     //CONST URL = 'https://vendor.alyamanlive.com';
     // CONST DB_PREFIX = "fby_";
-    CONST DB_PREFIX = "Al_";
+    CONST DB_PREFIX = "AL_";
     CONST URL = 'https://test.alyamanlive.com';
     CONST IMAGE_URL = 'http://test.alyamanlive.com/';
     CONST IMAGE_URL_UPLOADS = "http://test.alyamanlive.com/wp-content/uploads/";
