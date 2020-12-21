8ea7678b89eaf5c44ce626785bae9fe6ba77de1f
Lama Sonmez <lamasonmez@gmail.com>
2020-12-21 22:01:57 +0200

    fix

--- resources/views/supplier/products/index.blade.php
+++ resources/views/supplier/products/index.blade.php
@@ -10,7 +10,7 @@
             <div class="card-header flex-wrap border-0 pt-6 pb-0">
                 <div class="card-title">
                     <h3 class="card-label">المنتجات
-                    <span class="d-block text-muted pt-2 font-size-sm">المنتحات الخاصة بك</span></h3>
+                    <span class="d-block text-muted pt-2 font-size-sm">المنتجات الخاصة بك</span></h3>
                 </div>
                 <div class="card-toolbar">
 
