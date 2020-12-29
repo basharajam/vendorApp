1b8f98c27614652c6aa6ebef3b2c507b77c3ca86
Lama Sonmez <lamasonmez@gmail.com>
2020-12-29 20:44:02 +0200

    fix

--- app/Services/Supplier/SupplierService.php
+++ app/Services/Supplier/SupplierService.php
@@ -44,20 +44,20 @@ class SupplierService extends BaseService implements ISupplierService
         $supplier =  $this->repository->create($request->all());
 
         if(isset($request->national_id_image))
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->national_id_image))->toMediaCollection('national_id_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->national_id_image))->toMediaCollection('national_id_images');
         if(isset($request->passport_image))
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->passport_image))->toMediaCollection('passport_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->passport_image))->toMediaCollection('passport_images');
 
         if(isset($request->visa_image))
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->visa_image))->toMediaCollection('visa_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->visa_image))->toMediaCollection('visa_images');
         if(isset($request->commercial_license_image)){
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->commercial_license_image))->toMediaCollection('commercial_license_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->commercial_license_image))->toMediaCollection('commercial_license_images');
         }
         if(isset($request->company_logo)){
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->company_logo))->toMediaCollection('company_logo_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->company_logo))->toMediaCollection('company_logo_images');
         }
         if(isset($request->license_images)){
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->license_images))->toMediaCollection('license_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->license_images))->toMediaCollection('license_images');
         }
 
 
@@ -77,20 +77,20 @@ class SupplierService extends BaseService implements ISupplierService
         $supplier =  $this->repository->find($id);
         $supplier->update($request->all());
         if(isset($request->national_id_image))
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->national_id_image))->toMediaCollection('national_id_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->national_id_image))->toMediaCollection('national_id_images');
         if(isset($request->passport_image))
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->passport_image))->toMediaCollection('passport_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->passport_image))->toMediaCollection('passport_images');
 
         if(isset($request->visa_image))
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->visa_image))->toMediaCollection('visa_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->visa_image))->toMediaCollection('visa_images');
         if(isset($request->commercial_license_image)){
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->commercial_license_image))->toMediaCollection('commercial_license_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->commercial_license_image))->toMediaCollection('commercial_license_images');
         }
         if(isset($request->company_logo)){
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->company_logo))->toMediaCollection('company_logo_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->company_logo))->toMediaCollection('company_logo_images');
         }
         if(isset($request->license_images)){
-            $supplier->addMedia(public_paht('tmp/uploads/' . $request->license_images))->toMediaCollection('license_images');
+            $supplier->addMedia(public_path('tmp/uploads/' . $request->license_images))->toMediaCollection('license_images');
         }
         return $supplier;
     }
