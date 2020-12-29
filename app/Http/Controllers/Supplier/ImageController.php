1f0dce16976dbfc026cedaa27ba625ac4d738458
Lama Sonmez <lamasonmez@gmail.com>
2020-12-29 20:42:36 +0200

    modify temp path fater storagel ink

--- app/Http/Controllers/Supplier/ImageController.php
+++ app/Http/Controllers/Supplier/ImageController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class ImageController extends Controller
 {
     public function uploadImage(Request $request){
-        $path = storage_path('app/public/tmp/uploads');
+        // $path = storage_path('app/public/tmp/uploads');
+        $path = public_path('tmp/uploads');
 
         if (!file_exists($path)) {
             mkdir($path, 0777, true);
--- app/Services/Supplier/SupplierService.php
+++ app/Services/Supplier/SupplierService.php
@@ -30,7 +30,7 @@ class SupplierService extends BaseService implements ISupplierService
     public function getSuppliersForManager($manager_id){
             return Supplier::where('manager_id',$manager_id)->get();
     }
-
+    //storage_path('app/public/
     public function store(Request $request)
     {
         if(isset($request->company_countries)){
@@ -44,20 +44,20 @@ class SupplierService extends BaseService implements ISupplierService
         $supplier =  $this->repository->create($request->all());
 
         if(isset($request->national_id_image))
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->national_id_image))->toMediaCollection('national_id_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->national_id_image))->toMediaCollection('national_id_images');
         if(isset($request->passport_image))
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->passport_image))->toMediaCollection('passport_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->passport_image))->toMediaCollection('passport_images');
 
         if(isset($request->visa_image))
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->visa_image))->toMediaCollection('visa_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->visa_image))->toMediaCollection('visa_images');
         if(isset($request->commercial_license_image)){
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->commercial_license_image))->toMediaCollection('commercial_license_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->commercial_license_image))->toMediaCollection('commercial_license_images');
         }
         if(isset($request->company_logo)){
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->company_logo))->toMediaCollection('company_logo_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->company_logo))->toMediaCollection('company_logo_images');
         }
         if(isset($request->license_images)){
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->license_images))->toMediaCollection('license_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->license_images))->toMediaCollection('license_images');
         }
 
 
@@ -77,20 +77,20 @@ class SupplierService extends BaseService implements ISupplierService
         $supplier =  $this->repository->find($id);
         $supplier->update($request->all());
         if(isset($request->national_id_image))
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->national_id_image))->toMediaCollection('national_id_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->national_id_image))->toMediaCollection('national_id_images');
         if(isset($request->passport_image))
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->passport_image))->toMediaCollection('passport_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->passport_image))->toMediaCollection('passport_images');
 
         if(isset($request->visa_image))
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->visa_image))->toMediaCollection('visa_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->visa_image))->toMediaCollection('visa_images');
         if(isset($request->commercial_license_image)){
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->commercial_license_image))->toMediaCollection('commercial_license_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->commercial_license_image))->toMediaCollection('commercial_license_images');
         }
         if(isset($request->company_logo)){
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->company_logo))->toMediaCollection('company_logo_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->company_logo))->toMediaCollection('company_logo_images');
         }
         if(isset($request->license_images)){
-            $supplier->addMedia(storage_path('app/public/tmp/uploads/' . $request->license_images))->toMediaCollection('license_images');
+            $supplier->addMedia(public_paht('tmp/uploads/' . $request->license_images))->toMediaCollection('license_images');
         }
         return $supplier;
     }
